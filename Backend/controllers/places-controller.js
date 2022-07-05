d1173ab9a7df30696495bc3540d34f4f1c1a26ea
p00149021@houstonisd.org Mag17615@7 <lioneljones5116@gmail.com>
2022-07-05 12:36:19 -0500

    Finished out CRUD operations for Places

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -18,30 +18,44 @@ let DUMMY_PLACES = [
     }
  ];
 
-const getPlaceById = (req,res,next) => {
+const getPlaceById = async  (req,res,next) => {
     const placeID = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeID;
-    });
+    //const place = Place.findById(placeID).exec()   --if you wanted to get a real promise back (use exec)
+   
+    let place;
+    try {
+       place = await Place.findById(placeID);
+    }
+    catch (error) {
+        return res.status(500).json({message: 'There was a problem fetching the place by ID.'});
+    }
 
-   if(!place) {
-    return res.status(404).json({message: 'Could not find a place for the provided id.'});
-   }
+    if(!place) {
+        return res.status(404).json({message: 'Could not find a place for the provided id.'});
+    }
 
-    res.json({place});
+    //res.json({place});
+    res.json({place: place.toObject({getters: true})}); //we are using this syntax to bring back the _id as id on the returned result
 }
 
-const getPlacesByUserId = (req,res,next) => {
+const getPlacesByUserId = async (req,res,next) => {
     const userId = req.params.uid;
-    const places = DUMMY_PLACES.filter(p => {
-        return p.creator === userId;
-    });
-
+    
+    let places;
+    try{
+        places = await Place.find({creator : userId}); //returns an array
+    }
+    catch(error)
+    {
+        return res.status(500).json({message: error});
+    }
+    
     if(!places || places.length === 0) {
         return res.status(404).json({message: 'Could not find places for the provided  user id.'});
     }
 
-    res.json({places});
+    //res.json({places});
+    res.json({places: places.map(place => place.toObject({getters:true}))}); //we are using this syntax to bring back the _id as id on the returned result
 }
 
 const createPlace = async (req,res,next) => {
@@ -73,7 +87,7 @@ const createPlace = async (req,res,next) => {
   
 };
 
-const updatePlace = (req,res,next) => {
+const updatePlace = async (req,res,next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
      return res.status(500).json({message: 'Failed to update a place, missing required field information.'});
@@ -81,22 +95,44 @@ const updatePlace = (req,res,next) => {
 
     const {title,description} = req.body;
     const placeId = req.params.pid;
-    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId)};
-    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-    updatedPlace.title = title;
-    updatedPlace.description = description;
 
-    DUMMY_PLACES[placeIndex] = updatedPlace;
-    res.status(200).json({place: updatedPlace})
-}
+    let place;
+    try{
+        place = await Place.findById(placeId)
+    }
+    catch(error) {
+        return res.status(500).json({message: error});
+    }
 
-const deletePlace = (req,res,next) => {
-    const placeId = req.params.pid;
-    if(!DUMMY_PLACES.find(p => p.id === placeId)) {
-        return res.status(500).json({message: 'Place does not exist to delete!!!.'});
+    place.title = title;
+    place.description = description;
+
+    try{
+        await place.save();
+    }
+    catch(error) {
+        return res.status(500).json({message: 'Could not update place.. something went wrong'});
     }
     
-    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+
+    //res.status(200).json({place: place})
+    res.status(200).json({place: place.toObject({getters: true})}); //we are using this syntax to bring back the _id as id on the returned result
+}
+
+const deletePlace = async (req,res,next) => {
+    const placeId = req.params.pid;
+    let place;
+    try {
+       place = await Place.findById(placeId);
+       if(!place) {
+        return res.status(404).json({message: 'Could not find a place for the provided id.'});
+       }
+       await place.remove();
+    }
+    catch (error) {
+        return res.status(500).json({message: 'Problem deleting a pace.'});
+    }
+
     res.status(200).json({message:placeId + ' Deleted'})
 }
 
